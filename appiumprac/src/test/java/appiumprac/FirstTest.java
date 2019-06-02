4d3887d253d1712636867712c490373fc7c79f68
Sharathu <Sharathu@NGBLR-00735.Portal01.nextgen.com>
2019-06-02 10:45:02 +0530

    Changes to FirstTest.java to include Chrome browser.

--- appiumprac/src/test/java/appiumprac/FirstTest.java
+++ appiumprac/src/test/java/appiumprac/FirstTest.java
@@ -5,10 +5,12 @@ import java.net.URL;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
 
 public class FirstTest {
 
@@ -20,17 +22,21 @@ public class FirstTest {
 	
 	public static void openCalculator() throws Exception
 	{
+		System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\Software\\chromedriver.exe");
 		DesiredCapabilities cap= new DesiredCapabilities();
 		cap.setCapability("deviceName", "Android SDK built for x86");
 		//cap.setCapability("udid", "emulator-5554");
-		cap.setCapability("udid", "10.145.14.47:5555");
+		cap.setCapability("udid", "52039672f0a1a343");
 		cap.setCapability("platformName", "Android");
 		cap.setCapability("platformVersion", "7.1.1");
 		cap.setCapability("appPackage", "com.sec.android.app.popupcalculator");
 		cap.setCapability("appActivity", "com.sec.android.app.popupcalculator.Calculator");
-		
+		//cap.setCapability(CapabilityType.BROWSER_NAME, "chrome");
+		cap.setCapability("Appium --chromedriver-executable", "E:\\Selenium\\Software\\chromedriver.exe");
+				
 		URL url= new URL("http://127.0.0.1:4723/wd/hub");
-		driver= new AppiumDriver<MobileElement>(url,cap);
+		driver= new AndroidDriver<MobileElement>(url,cap);
+		//driver.get("https://google.com");
 		
 		System.out.println("Application is launched");
 		
@@ -43,15 +49,12 @@ public class FirstTest {
 		plus.click();
 		three.click();
 		two.click();
-		System.out.println(result.getText());
-		
-		if(result.getText().equals("55"))
+		System.out.println(result.getText());		
+		if(Integer.parseInt(result.getText())==55)
 			System.out.println("Result is correctly displayed");
 		else
 			System.out.println("Wrong result");
 		driver.quit();
-		
-		
 	}
 }
 
